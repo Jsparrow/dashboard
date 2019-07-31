fcf169b8865451a1f7b003d66c922393776fdecf
Matthias Webhofer <matthias.webhofer@splendit.at>
2019-07-31 16:04:36 +0200

    SIM-1605 UPDATE fix plugin declaration

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,12 +9,14 @@ module.exports = {
             href: '/logo.png'
         }]
     ],
-    plugins: [ 
-        '@vuepress/google-analytics', 
-        { 
-            'ga': 'UA-144352955-2' 
-        } 
-    ], 
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-144352955-2'
+            }
+        ]
+    ],
     extendMarkdown(md) {
         toc: {
             includeLevel: [2, 3, 4]
